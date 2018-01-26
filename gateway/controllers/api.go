6c2d13400e7bca8a62a487b470d9eb3ad3055ed8
liuxp <liuxp0827@foxmail.com>
2018-01-26 15:59:39 +0800

    11

--- gateway/controllers/api.go
+++ gateway/controllers/api.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"git.oschina.net/kuaishangtong/common/utils/log"
 	"git.oschina.net/kuaishangtong/navi/gateway/api"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/context"
@@ -23,41 +24,12 @@ func (this *ApiController) Proxy() {
 	api := api.GlobalApiManager.GetApi(service)
 	if api != nil {
 		if _, ok := api.ServerURLs[apiurl]; ok {
-			//transport := http.DefaultTransport
-			//
-			//// step 1
-			//outReq := new(http.Request)
-			//*outReq = *this.Ctx.Request // this only does shallow copies of maps
-			//
-			//if clientIP, _, err := net.SplitHostPort(this.Ctx.Request.RemoteAddr); err == nil {
-			//	if prior, ok := outReq.Header["X-Forwarded-For"]; ok {
-			//		clientIP = strings.Join(prior, ", ") + ", " + clientIP
-			//	}
-			//	outReq.Header.Set("X-Forwarded-For", clientIP)
-			//}
-			//
-			//// step 2
-			//res, err := transport.RoundTrip(outReq)
-			//if err != nil {
-			//	this.Ctx.ResponseWriter.WriteHeader(http.StatusBadGateway)
-			//	return
-			//}
-			//
-			//// step 3
-			//for key, value := range res.Header {
-			//	for _, v := range value {
-			//		this.Ctx.ResponseWriter.Header().Add(key, v)
-			//	}
-			//}
-			//
-			//this.Ctx.ResponseWriter.WriteHeader(res.StatusCode)
-			//io.Copy(this.Ctx.ResponseWriter, res.Body)
-			//res.Body.Close()
-
 			director := func(req *http.Request) {
 				req = this.Ctx.Request
 				req.URL.Scheme = "http"
-				req.URL.Host = api.Cluster.Select(service+"/"+apiurl, req.Method)
+				host := api.Cluster.Select(service+"/"+apiurl, req.Method)
+				log.Debugf("service %s api %s, host %s", service, api, host)
+				req.URL.Host = host
 			}
 			proxy := &httputil.ReverseProxy{Director: director}
 			proxy.ServeHTTP(this.Ctx.ResponseWriter, this.Ctx.Request)
