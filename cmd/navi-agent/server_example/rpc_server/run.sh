5915e7c0c22a6683df44f24ce213d5977dd508cf
liuxp <liuxp0827@foxmail.com>
2018-02-28 13:41:27 +0800

    11

--- cmd/navi-agent/server_example/rpc_server/run.sh
+++ cmd/navi-agent/server_example/rpc_server/run.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-g++ -Wall -O3 -g -DHAVE_NETINET_IN_H -I. -I/usr/local/include/  -L/usr/local/lib/ *.cpp  -o rpc-server -lthriftnb -levent -lthrift
-#g++ -o asv-rpc *.cpp -DHAVE_NETINET_IN_H -I. -I/usr/local/include/ -I/kaldi/kaldi/kaldi/src/kvp/ -L/usr/local/lib/ -lthriftnb -lthrift -L/kaldi/kaldi/kaldi/src/lib/ -lkvp-asv -lfaiss -L/usr/OpenBLAS/lib -lopenblas -levent -lrt -fPIC -m64 -Wall -g -O3 -mavx -msse4 -mpopcnt -Wno-sign-compare -std=c++11
-#g++ -g -DHAVE_NETINET_IN_H -I. -I/usr/local/include/ -L/usr/local/lib/  kvpClient.cc -o rpc-client -lthriftnb -levent -lthrift -lrt
-#export LD_LIBRARY_PATH=/kaldi/kaldi/kaldi/src/lib:/usr/local/lib
+g++ -Wall -O3 -g -DHAVE_NETINET_IN_H -I. -I/usr/local/include/ -L/usr/local/lib/ *.cpp  -o rpc-server -lthriftnb -levent -lthrift -lrt -lz
 #./asv-rpc
