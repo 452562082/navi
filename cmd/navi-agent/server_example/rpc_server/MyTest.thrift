3a8cc30bc2a6bb15e437fc151febf2f8fa752efc
liuxp <liuxp0827@foxmail.com>
2018-02-28 13:44:48 +0800

    11

--- cmd/navi-agent/server_example/rpc_server/MyTest.thrift
+++ cmd/navi-agent/server_example/rpc_server/MyTest.thrift
@@ -8,14 +8,6 @@ struct Response {
     2:required string responseJSON;
 }
 
-# 异常描述定义，当服务提供者处理过程出现异常时，向服务调用者返回
-exception ServiceException {
-    # EXCCODE 是异常代码，也是一个int32型。
-    1:required i32 exceptionCode;
-    # 异常的描述信息，使用字符串进行描述
-    2:required string exceptionMeg;
-}
-
 # 这是经过泛化后的Apache Thrift接口
 service MyTest {
 
@@ -28,7 +20,7 @@ service MyTest {
 		# rpc server必须实现的接口，说明该server是以什么模式运行，分为dev和prod；dev为开发版本，prod为生产版本
         string ServiceMode(),
 
-        Response SayHello(1:string yourName) throws (1:required ServiceException e)
+        Response SayHello(1:string yourName)
 
-        Response SaveWave(1:string fileName, 2:string wavFormat, 3:binary data) throws (1:required ServiceException e)
+        Response SaveWave(1:string fileName, 2:string wavFormat, 3:binary data)
 }
