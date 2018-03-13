6eff22f1faa6a6cb281f7e9729b366d14d6698e3
liuxp <liuxp0827@foxmail.com>
2018-03-13 15:10:00 +0800

    11

--- cmd/navi-agent/update_agent_config.sh
+++ cmd/navi-agent/update_agent_config.sh
@@ -4,34 +4,30 @@ ZOOKEEPER_HOSTS=$(echo $ZK_HOSTS)
 
 SERVER_HOSTS=$(echo $SERVER_HOSTS)
 
-echo ZOOKEEPER_HOSTS = ZOOKEEPER_HOSTS
+echo ZOOKEEPER_HOSTS = ${ZOOKEEPER_HOSTS}
 
 
 update_zookeeper_hosts() {
-    local ret=1
 
     if [ "${ZOOKEEPER_HOSTS}" != "" ]; then
         sed -i 's/\("zookeeper_hosts": "\).*/\1'"${ZOOKEEPER_HOSTS}"'",/g' cfg.json
-
         echo "update zookeeper_hosts to ${ZOOKEEPER_HOSTS} success"
-        ret=0
-		return ret
+		return 0
     else
         echo "environment variable 'ZK_HOSTS' is not set"
-	    return ret
+	    return 1
     fi
 }
 
 update_server_hosts() {
-     local ret=1
 
     if [ "${SERVER_HOSTS}" != "" ];then
         sed -i 's/\("server_hosts": "\).*/\1'"${SERVER_HOSTS}"'",/g' cfg.json
-        ret=0
-		return ret
+        echo "update server_hosts_hosts to ${SERVER_HOSTS} success"
+		return 0
     else
          echo "environment variable 'SERVER_HOSTS' is not set"
-	    return ret
+	    return 1
     fi
 }
 
