7cce3be4e08912cf0ce7b6ba87e3bd7702b6ba59
liuxp <liuxp0827@foxmail.com>
2018-03-13 20:14:25 +0800

    11

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,4 +7,6 @@ nohup /rpc/bin/mytest 9292 >/rpc/logs/mytest.log 2>&1 &
 sleep 3
 echo "start navi-agent"
 
-/rpc/bin/navi-agent -c /rpc/etc/cfg.json
+nohup /rpc/bin/navi-agent -c /rpc/etc/cfg.json >/rpc/logs/agent.log 2>&1 &
+
+sleep 1h
--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker run -d --env-file=env.ini -p9292:9292 -v ./rpc:/rpc --name:MyTest mytest:alpha
+docker run -d --env-file=env.ini -p9292:9292 -v /opt/golang/src/kuaishangtong/navi/scripts/rpc:/rpc --name=MyTest mytest:alpha
 #docker run -it --env-file=env.ini -p9292:9292 -v ./rpc:/rpc mytest:alpha /bin/bash
 # /rpc/update_config.sh agent /rpc/etc/cfg.json
 # /rpc/run.sh
