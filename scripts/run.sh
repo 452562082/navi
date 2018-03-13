6360ff39e606ef2629639e9f0526a4edd10d68ab
liuxp <liuxp0827@foxmail.com>
2018-03-13 19:44:19 +0800

    11

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,4 @@ export LD_LIBRARY_PATH=/usr/local/lib:/rpc/libs
 
 nohup /rpc/bin/mytest 9292 >/rpc/logs/mytest.log 2>&1 &
 sleep 3
-/rpc/bin/navi-agent -c /rpc/etc/cfg.json >/rpc/logs/agent.log 2>&1
+/rpc/bin/navi-agent -c /rpc/etc/cfg.json
