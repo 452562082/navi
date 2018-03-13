31041fa06a3aeebe56072ad5f0a2e14c3a97f2db
liuxp <liuxp0827@foxmail.com>
2018-03-13 21:34:22 +0800

    11

--- scripts/rpcservice/agent_docker_build.sh
+++ scripts/rpcservice/agent_docker_build.sh
@@ -13,12 +13,6 @@ sh -c ./run.sh
 # 将 MyTest 拷贝到 rpc/bin 下
 cp -a ./mytest $GOPATH/src/kuaishangtong/navi/scripts/rpcservice/rpc/bin
 
-# 将 MyTest 依赖库拷贝到 rpc/libs 下
-cp -a /usr/local/lib/libthriftnb-0.10.0.so rpc/libs
-cp -a /usr/lib64/libevent-2.0.so.5 rpc/libs
-cp -a /usr/lib64/libevent-2.0.so.5.1.9 rpc/libs
-cp -a /usr/local/lib/libthrift-0.10.0.so rpc/libs
-
 # 编译 navi-agent
 cd  $GOPATH/src/kuaishangtong/navi/cmd/navi-agent
 go build -v
@@ -33,6 +27,12 @@ cp -a $GOPATH/src/kuaishangtong/navi/scripts/rpcservice/run.sh rpc/
 cp -a $GOPATH/src/kuaishangtong/navi/cmd/navi-agent/navi-agent rpc/bin
 
 cp -a $GOPATH/src/kuaishangtong/navi/cmd/navi-agent/cfg.json rpc/etc
+
+# 将 MyTest 依赖库拷贝到 rpc/libs 下
+cp -a /usr/local/lib/libthriftnb-0.10.0.so rpc/libs
+cp -a /usr/lib64/libevent-2.0.so.5 rpc/libs
+cp -a /usr/lib64/libevent-2.0.so.5.1.9 rpc/libs
+cp -a /usr/local/lib/libthrift-0.10.0.so rpc/libs
 
 echo "FROM centos:7
 COPY ./rpc /rpc
