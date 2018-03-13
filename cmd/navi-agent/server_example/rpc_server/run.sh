6894b149b83a3cfd8533065549728d5432cbfdd8
liuxp <liuxp0827@foxmail.com>
2018-03-13 17:20:22 +0800

    11

--- cmd/navi-agent/server_example/rpc_server/run.sh
+++ cmd/navi-agent/server_example/rpc_server/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-g++ -Wall -O3 -g -DHAVE_NETINET_IN_H -I. -I/usr/local/include/ -L/usr/local/lib/ *.cpp  -o rpc-server -lthriftnb -levent -lthrift -lrt -lz
+g++ -Wall -O3 -g -DHAVE_NETINET_IN_H -I. -I/usr/local/include/ -L/usr/local/lib/ *.cpp  -o mytest -lthriftnb -levent -lthrift -lrt -lz
 #./asv-rpc
---
+++ scripts/make_agent_docker.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+mkdir mytest
+
+sh ../cmd/navi-agent/server_example/rpc_server/run.sh
+
+cp -a ../cmd/navi-agent/server_example/rpc_server/mytest mytest/
+
+cd ../cmd/navi-agent
+
+go build -v
+
+cd ../../scripts
+
+cp -a ./update_config.sh mytest/
+
+cp -a ../cmd/navi-agent/navi-agent mytest/
+
+cp -a ../cmd/navi-agent/cfg.json mytest/
+
+echo "FROM busybox
+COPY ./mytest /go/src/mytest
+ENTRYPOINT [\"sh\", \"-c\", \"/go/src/mytest/update_config.sh\"]
+CMD [\"sh\", \"-c\", \"/go/src/mytest/run.sh\"]
+" > Dockerfile
