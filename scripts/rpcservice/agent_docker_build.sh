faaba75a6f470cce8e72f37b7bd2469b3d87c5dd
liuxp <liuxp0827@foxmail.com>
2018-03-13 21:32:18 +0800

    11

--- scripts/rpcservice/agent_docker_build.sh
+++ scripts/rpcservice/agent_docker_build.sh
@@ -1,23 +1,31 @@
 #!/bin/sh
 
-mkdir -p rpc/logs
-mkdir -p rpc/libs
-mkdir -p rpc/bin
-mkdir -p rpc/etc
+mkdir -p $GOPATH/src/kuaishangtong/navi/scripts/rpcservice/rpc/logs
+mkdir -p $GOPATH/src/kuaishangtong/navi/scripts/rpcservice/rpc/libs
+mkdir -p $GOPATH/src/kuaishangtong/navi/scripts/rpcservice/rpc/bin
+mkdir -p $GOPATH/src/kuaishangtong/navi/scripts/rpcservice/rpc/etc
 
-
-chmod 777 ../../cmd/navi-agent/server_example/rpc_server/run.sh
-cd ../../cmd/navi-agent/server_example/rpc_server
+# 编译rpc_server MyTest
+chmod 777 $GOPATH/src/kuaishangtong/navi/cmd/navi-agent/server_example/rpc_server/run.sh
+cd $GOPATH/src/kuaishangtong/navi/cmd/navi-agent/server_example/rpc_server
 sh -c ./run.sh
 
-cp -a ./mytest $GOPATH/src/kuaishangtong/navi/scripts/rpc/bin
+# 将 MyTest 拷贝到 rpc/bin 下
+cp -a ./mytest $GOPATH/src/kuaishangtong/navi/scripts/rpcservice/rpc/bin
 
+# 将 MyTest 依赖库拷贝到 rpc/libs 下
+cp -a /usr/local/lib/libthriftnb-0.10.0.so rpc/libs
+cp -a /usr/lib64/libevent-2.0.so.5 rpc/libs
+cp -a /usr/lib64/libevent-2.0.so.5.1.9 rpc/libs
+cp -a /usr/local/lib/libthrift-0.10.0.so rpc/libs
+
+# 编译 navi-agent
 cd  $GOPATH/src/kuaishangtong/navi/cmd/navi-agent
-
 go build -v
 
 cd $GOPATH/src/kuaishangtong/navi/scripts/rpcservice
 
+# 将 脚本，navi-agent 及配置文件拷贝到 rpc 下
 cp -a $GOPATH/src/kuaishangtong/navi/scripts/update_config.sh rpc/
 
 cp -a $GOPATH/src/kuaishangtong/navi/scripts/rpcservice/run.sh rpc/
@@ -25,13 +33,6 @@ cp -a $GOPATH/src/kuaishangtong/navi/scripts/rpcservice/run.sh rpc/
 cp -a $GOPATH/src/kuaishangtong/navi/cmd/navi-agent/navi-agent rpc/bin
 
 cp -a $GOPATH/src/kuaishangtong/navi/cmd/navi-agent/cfg.json rpc/etc
-
-cp -a /usr/local/lib/libthriftnb-0.10.0.so rpc/libs
-cp -a /usr/lib64/libevent-2.0.so.5 rpc/libs
-cp -a /usr/lib64/libevent-2.0.so.5.1.9 rpc/libs
-cp -a /usr/local/lib/libthrift-0.10.0.so rpc/libs
-
-
 
 echo "FROM centos:7
 COPY ./rpc /rpc
