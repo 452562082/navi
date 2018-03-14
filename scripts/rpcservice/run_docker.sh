d9518db9800fbd4e5ca79398adcff2eb312ca94c
liuxp <liuxp0827@foxmail.com>
2018-03-14 09:19:34 +0800

    11

--- scripts/rpcservice/run_docker.sh
+++ scripts/rpcservice/run_docker.sh
@@ -2,6 +2,6 @@
 
 docker run -d --env-file=../env.ini -p9292:9292 --name=MyTest -v /opt/golang/src/kuaishangtong/navi/scripts/rpc:/rpc mytest:alpha
 docker run --env-file=../env.ini -p9292:9292 -v /opt/golang/src/kuaishangtong/navi/scripts/rpc:/rpc mytest:alpha
-#docker run -it --env-file=../env.ini -p9292:9292 -v ./rpc:/rpc mytest:alpha /bin/bash
+#docker run -it --env-file=../env.ini -p9292:9292 -v /opt/golang/src/kuaishangtong/navi/scripts/rpc:/rpc mytest:alpha /bin/bash
 # /rpc/update_config.sh agent /rpc/etc/cfg.json
 # /rpc/run.sh
