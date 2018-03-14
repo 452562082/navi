3f58fa71efd9c14d9926b5236de3b358fd42161a
yuwenlong <yuwenlong@kuaishang.com.cn>
2018-03-14 17:18:52 +0800

    123

--- scripts/httpservice/run_docker.sh
+++ scripts/httpservice/run_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --env-file=../env.ini -p9292:9292 -v /opt/golang/src/kuaishangtong/navi/scripts/rpc:/rpc mytest_http:alpha
+docker run --env-file=../env.ini -p8081:8081 -v /opt/golang/src/kuaishangtong/navi/scripts/rpc:/rpc mytest_http:alpha
