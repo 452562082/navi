41d3035232e2c440ef48dc4ab51497060f632585
liuxp <liuxp0827@foxmail.com>
2018-03-14 15:30:48 +0800

    11

--- scripts/rpcservice/agent_docker_build.sh
+++ scripts/rpcservice/agent_docker_build.sh
@@ -34,10 +34,10 @@ cp -a /usr/lib64/libevent-2.0.so.5 rpc/libs
 cp -a /usr/lib64/libevent-2.0.so.5.1.9 rpc/libs
 cp -a /usr/local/lib/libthrift-0.10.0.so rpc/libs
 
-#echo "FROM centos:7
-#COPY rpc /rpc
-#RUN chmod 777 /rpc/*.sh
-##CMD [\"/rpc/run.sh\"]
-#" > Dockerfile
+echo "FROM centos:7
+COPY rpc /rpc
+RUN chmod 777 /rpc/*.sh
+CMD [\"/rpc/run.sh\"]
+" > Dockerfile
 #
-#docker build -t mytest:alpha .
+docker build -t mytest:alpha .
