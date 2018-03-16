2d074754c26da228fd54079c17115f5b51dd9593
yuwenlong <yuwenlong@kuaishang.com.cn>
2018-03-16 11:57:47 +0800

    123

--- scripts/httpservice/run_docker.sh
+++ scripts/httpservice/run_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker run --env-file=../env.ini -p8081:8081 -v /opt/golang/src/kuaishangtong/navi/scripts/httpservice/navi:/navi mytest_http:alpha
-
+#docker run --env-file=../env.ini -p8081:8081 -v /opt/golang/src/kuaishangtong/navi/scripts/httpservice/navi:/navi mytest_http:alpha
+docker-compose up
